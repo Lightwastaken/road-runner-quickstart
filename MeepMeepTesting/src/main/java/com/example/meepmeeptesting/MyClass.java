c5f5464bb8383695083ba09a593e96fd7de6ac41
LightwasTaken <Johnyang3794@gmail.com>
2023-03-09 18:12:14 -0800

    meep meep updates

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
@@ -1,4 +1,38 @@
 package com.example.meepmeeptesting;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
 public class MyClass {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(52.48180821614297, 52.48180821614297, Math.toRadians(180), Math.toRadians(180), 24)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, 60, Math.toRadians(90)))
+                                .lineToLinearHeading(new Pose2d(-34,12,Math.toRadians(45)))
+                                // preload [ not implmented]
+                                .waitSeconds(0.5)
+                                .lineToLinearHeading(new Pose2d(-57, 12, Math.toRadians(180)))
+                                // goes to cone stack ^
+                                .waitSeconds(0.5)
+                                .lineToLinearHeading(new Pose2d(-34,12,Math.toRadians(45)))
+                                .build()
+
+                );
+
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
 }
